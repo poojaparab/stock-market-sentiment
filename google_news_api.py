e804f79fa40925450dc8d3b7485b229b4dec3766
poojaparab <pooja.parab@quantiphi.com>
2021-09-13 18:09:37 +0530

    imporved dates

--- google_news_api.py
+++ google_news_api.py
@@ -1,9 +1,14 @@
 from GoogleNews import GoogleNews
 from newspaper import Article
 import pandas as pd
+from datetime import date, timedelta
 
 def get_news():
-    googlenews=GoogleNews(start='05/17/2020',end='05/10/2020')
+    current_date = date.today().strftime("%m/%d/%Y")
+    end_date = date.today() - timedelta(days=7)
+    end_date = end_date.strftime("%m/%d/%Y")
+    # print(current_date, end_date)
+    googlenews=GoogleNews(current_date, end_date)
     googlenews.search('itc share')
     result=googlenews.result()
     df=pd.DataFrame(result)
